53a5aca339d62452b39208b7534a183e24a2af82
YDemirbuken <yasarcan.demirbuken@ats-global.com>
2026-05-18 00:49:37 +0300

    Category page and forms added.

---
+++ StoreFlow/Extensions/CategoryStatusExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace StoreFlow.Extensions;
+
+public static class CategoryStatusExtensions
+{
+    public static string GetCategoryBadgeClass(this string? status)
+    {
+        return status switch
+        {
+            "True" => "badge-success",
+            "False" => "badge-secondary",
+            _ => "badge-secondary"
+        };
+    }
+
+    public static string GetStatusText(this string? status)
+    {
+        return status switch
+        {
+            "True" => "Aktif",
+            "False" => "Pasif",
+            _ => "Pasif"
+        };
+    }
+}
---
+++ StoreFlow/Models/CategoryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using StoreFlow.Extensions;
+
+namespace StoreFlow.Models;
+
+public class CategoryViewModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string? Status { get; set; }
+    public string CategoryBadgeClass => Status.GetCategoryBadgeClass();
+    public string StatusText => Status.GetStatusText();
+}
