bd1173eb83dc49cdb971d39fc99699f6a42515c8
Yaşarcan Demirbüken <ycandemirbuken@gmail.com>
2026-05-18 00:49:37 +0300

    Category page and forms added.

---
+++ StoreFlow/Controllers/CategoryController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoreFlow.Context;
+using StoreFlow.Entities;
+using StoreFlow.Models;
+
+namespace StoreFlow.Controllers;
+
+public class CategoryController(StoreDbContext context) : Controller
+{
+    public async ValueTask<IActionResult> CategoryList()
+    {
+        var categories = await context.Categories.Select(c => new CategoryViewModel
+        {
+            Id = c.Id,
+            Name = c.Name,
+            Status = c.Status.ToString()
+        }).ToListAsync();
+
+        return View(categories);
+    }
+
+    [HttpGet]
+    public async ValueTask<IActionResult> CreateCategory()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async ValueTask<IActionResult> CreateCategory(Category category)
+    {
+        category.Status = false;
+        context.Categories.Add(category);
+        await context.SaveChangesAsync();
+
+        return RedirectToAction(nameof(CategoryList));
+    }
+
+    [HttpGet]
+    public async ValueTask<IActionResult> UpdateCategory(int id)
+    {
+        var category = await context.Categories.FindAsync(id);
+        return View(category);
+    }
+
+    [HttpPost]
+    public async ValueTask<IActionResult> UpdateCategory(Category category)
+    {
+        context.Categories.Update(category);
+        await context.SaveChangesAsync();
+        return RedirectToAction(nameof(CategoryList));
+    }
+
+    public async ValueTask<IActionResult> DeleteCategory(int id)
+    {
+        var category = await context.Categories.FindAsync(id);
+        context.Categories.Remove(category);
+        await context.SaveChangesAsync();
+
+        return RedirectToAction(nameof(CategoryList));
+    }
+
+}
